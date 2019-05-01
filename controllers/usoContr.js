d91b6149cffe8babb6193adc6211f85e3803cc7a
Marcos Rivas <marcusstivenrobert@gmail.com>
2019-05-01 14:12:40 -0400

    servicio 7.a OK, ojo con fecha

--- controllers/usoContr.js
+++ controllers/usoContr.js
@@ -1,10 +1,30 @@
-const Uso = require('../models').Uso;
-const Cliente=require('../models').Cliente;
-const Concepto=require('../models').Concepto;
+const models=require('../models');
+
 module.exports = {
   getUso(req, res) {
-    return Uso.findAll({
-        include:[{model:Cliente, as: 'Cliente'},{model:Concepto, as: 'Concepto'}]
+      var where={
+
+      };
+      if(req.query.idCliente){
+          where={cliente_id:req.query.idCliente}
+      }else {
+          if (req.query.idConcepto) {
+              where = {concepto_id: req.query.idConcepto}
+          } else {
+              if (req.query.fechaInicio && req.query.fechaFin) {
+                  where = {
+                      [models.Sequelize.Op.and]: [{ fecha:{[models.Sequelize.Op.gte]: req.query.fechaInicio}}, {fecha:{[models.Sequelize.Op.lte]: req.query.fechaFin}}]
+                  }
+              }
+          }
+      }
+
+
+
+    return models.Uso.findAll({
+        attributes:['id','utilizado','fecha'],
+        include:[{model:models.Cliente, as: 'Cliente'},{model:models.Concepto, as: 'Concepto'}],
+        where:where
     })
       .then(usos => res.status(200).send(usos))
       .catch(error => {
