98fdbc100f44f2bb4b30c155aa1083013f515d66
Marcos Rivas <marcusstivenrobert@gmail.com>
2019-05-06 17:16:29 -0400

    set fechaCaducidad por paramDuracion

--- controllers/clienteContr.js
+++ controllers/clienteContr.js
@@ -1,87 +1,95 @@
 const Cliente = require('../models').Cliente;
 const Bolsa = require('../models').Bolsa;
 const Regla = require('../models').Regla;
+const ParamDuracion = require('../models').ParamDuracion;
 const Op = require('../models').Sequelize.Op;
 const sequelize = require('../models').sequelize;
 
 module.exports = {
 
-  list(req, res) {
-    Cliente.findAll()
-      .then(
-        (clientes) => {
-          res.status(200).send(clientes);
-        }
-      )
-      .catch(
-        (error) => {
-          res.status(400).send(error);
-        }
-      );
-  },
+    list(req, res) {
+        Cliente.findAll()
+            .then(
+                (clientes) => {
+                    res.status(200).send(clientes);
+                }
+            )
+            .catch(
+                (error) => {
+                    res.status(400).send(error);
+                }
+            );
+    },
 
-  //servicio 8.a
-  addPuntos(req, res) {
-    return Regla.findAll(
-      {
-        where: {
-          [Op.and]: [{ limInferior: { [Op.lte]: req.body.monto } }, { limSuperior: { [Op.gt]: req.body.monto } }]
-        }
-      }
-    )
-      .then(reglas => {
-        var puntosCalculados = 0;
-        var fechaAhora = new Date();
-        //para vencimiento en 30 dias
-        fechaAhora.setDate(fechaAhora.getDate() + 30);
-
-        reglas.forEach(function (valor, indice, array) {
-
-          puntosCalculados = puntosCalculados + Math.floor(req.body.monto / valor.equivalencia);
-
-
-        });
-        // Asignar puntaje segun reglas
-        return Bolsa.create(
-          {
-            cliente_id: req.body.idCliente,
-            fechaCaducidad: fechaAhora.toString(),
-            asignado: puntosCalculados,
-            saldo: puntosCalculados,
-            montoOp: req.body.monto
-          }
+    //servicio 8.a
+    addPuntos(req, res) {
+        return Regla.findAll(
+            {
+                where: {
+                    [Op.and]: [{limInferior: {[Op.lte]: req.body.monto}}, {limSuperior: {[Op.gt]: req.body.monto}}]
+                }
+            }
         )
-          .then((bolsa) => res.status(201).send(bolsa))
-          .catch((error) => res.status(500).send(error))
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send('error del servidor');
-      });
-  },
+            .then(reglas => {
+                var puntosCalculados = 0;
+                var fechaAhora = new Date();
 
-  //servicio 8.c
-  getPuntosDeMonto(req, res) {
-    return Regla.findAll(
-      {
-        where: {
-          [Op.and]: [{ limInferior: { [Op.lte]: req.query.monto } }, { limSuperior: { [Op.gte]: req.query.monto } }]
-        }
-      }
-    )
-      .then(reglas => {
-        var puntosCalculados = 0;
-        reglas.forEach(function (valor, indice, array) {
+                //calcular fecha de caducidad de acuerdo a la fecha de asignacion
+                var duracionDias= 0;
+                ParamDuracion.findAll(
+                    where = {
+                        [Op.and]: [{ validez_ini: { [Op.lte]: fechaAhora } }, { validez_fin: { [Op.gte]: fechaAhora } }]
+                    }
+                )
+                    .then(paramDuracion=>{
+                        //calculo de puntos desde reglas
+                        puntosCalculados = puntosCalculados + Math.floor(req.body.monto / reglas[0].equivalencia);
+                        //para vencimiento en puntaje en dias
+                        var fechaVencimiento=new Date();
+                        fechaVencimiento.setDate(fechaAhora.getDate() + paramDuracion[0].duracion);
+                        // Asignar puntaje segun reglas
+                        return Bolsa.create(
+                            {
+                                cliente_id: req.body.idCliente,
+                                fechaCaducidad: fechaVencimiento.toString(),
+                                asignado: puntosCalculados,
+                                saldo: puntosCalculados,
+                                montoOp: req.body.monto
+                            }
+                        )
+                            .then((bolsa) => res.status(201).send(bolsa))
+                            .catch((error) => res.status(500).send(error))
+                    })
 
-          puntosCalculados = puntosCalculados + Math.floor(req.query.monto / valor.equivalencia);
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).send('error del servidor');
+            });
+    },
+
+    //servicio 8.c
+    getPuntosDeMonto(req, res) {
+        return Regla.findAll(
+            {
+                where: {
+                    [Op.and]: [{limInferior: {[Op.lte]: req.query.monto}}, {limSuperior: {[Op.gte]: req.query.monto}}]
+                }
+            }
+        )
+            .then(reglas => {
+                var puntosCalculados = 0;
+                reglas.forEach(function (valor, indice, array) {
+
+                    puntosCalculados = puntosCalculados + Math.floor(req.query.monto / valor.equivalencia);
 
 
-        });
-        res.status(200).send({ puntos: puntosCalculados })
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send('error del servidor');
-      });
-  }
+                });
+                res.status(200).send({puntos: puntosCalculados})
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).send('error del servidor');
+            });
+    }
 };
